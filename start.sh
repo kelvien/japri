690a80c6cc680d5b9fa3bc2ee741c7eabc374b05
kelvien <kelvienhidayat@gmail.com>
2016-05-25 21:58:31 -0500

    Update start script

--- start.sh
+++ start.sh
@@ -1,5 +1,4 @@
 #Place this in /etc/rc.local, this script is to repair mongo, run mongo and run node on Raspberry Pi's start boot up
 
 mongod --dbpath /home/pi/DB/mongodb --repair
-mongod --dbpath /home/pi/DB/mongodb --auth & 
-node /home/pi/prog/dist_test/japri/server.js &
+mongod --dbpath /home/pi/DB/mongodb --auth & node /home/pi/prog/dist_test/japri/server.js
